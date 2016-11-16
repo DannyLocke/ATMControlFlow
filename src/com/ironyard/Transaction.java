67d8339b9a815593abdb6e10723be8802702e9d8
Danny Locke <dlocke@mac.com>
2016-11-16 15:54:47 -0600

    ATMControlFlow

--- src/com/ironyard/Transaction.java
+++ src/com/ironyard/Transaction.java
@@ -1,5 +1,7 @@
 package com.ironyard;
 
+import java.io.*;
+
 /**
  * Created by dlocke on 11/16/16.
  */
@@ -9,6 +11,7 @@ public class Transaction {
     private String name;
     private int transaction;
     private int withdraw;
+    private int money = 100;
 
     //method to receive user name
     public void chooseName() throws Exception{
@@ -24,34 +27,48 @@ public class Transaction {
     }//end chooseName()
 
     //method to get user transaction choice
-    public void transactionChoice() throws Exception{
-        System.out.println("Enter 1 to check your balance, 2 to withdraw funds or 3 to cancel.");
-        transaction = Welcome.scanner.nextInt();
+    public void transactionChoice() throws Exception {
+
+        try {
+
+            System.out.println("Enter 1 to check your balance, 2 to withdraw funds or 3 to cancel.");
+            transaction = Welcome.scanner.nextInt();
+
+            if (transaction != 1 && transaction != 2 && transaction != 3) {
+                System.out.println("Only enter a '1', '2', or '3'.");
+            }
+
+        }//end try
+        catch (Exception e){
+            System.out.println("Invalid Input");
+        }//end catch
 
             //balance
-        if(transaction == 1) {
-            System.out.println("Your balance is $100.");
+            if (transaction == 1) {
+                System.out.println("Your balance is $100.");
 
-                  //withdrawal
-        }else if (transaction == 2){
-            System.out.println("How much money would you like to withdraw?");
-            withdraw = Welcome.scanner.nextInt();
+                //withdrawal
+            } else if (transaction == 2) {
+                System.out.println("How much money would you like to withdraw?");
+                withdraw = Welcome.scanner.nextInt();
 
                 //ensure sufficient funds
-                if (withdraw > 0 && withdraw <=100){
-                    System.out.println("Thank you. Here is your " + withdraw + " dollars. Please take your money.");
-                }else if (withdraw > 100){
+                if (withdraw > 0 && withdraw <= 100) {
+                    money = money - withdraw;
+                    System.out.println("Thank you. Here is your $" + withdraw + ".00 dollars. Please take your money.");
+                    System.out.println("Your new balance is: $" + money + ".00. Have a nice day!");
+                } else if (withdraw > 100) {
                     throw new Exception("Insufficient Funds.");
                 }
 
-        //cancel
-        }else if (transaction == 3){
-            System.out.println("Thank you and please come again.");
+                //cancel
+            } else if (transaction == 3) {
+                System.out.println("Thank you and please come again.");
 
-            //if a number other than 1, 2 or 3 is entered
-        } else throw new Exception("Invalid number entered.");
+                //if the entry is something other than 1, 2 or 3
+            } else throw new Exception("Only enter a '1', '2', or '3'.");
 
-    }//end transactionChoice()
+        }//end transactionChoice()
 
     public String getName() {
         return name;
